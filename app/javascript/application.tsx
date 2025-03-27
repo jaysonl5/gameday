170dd7d0328a84ad0843e71a2688ce118efa6883
Jayson <jayson.lewis5@gmail.com>
2025-03-27 12:28:33 -0500

    missed import removal

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import Provider from "./components/Provider";
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
