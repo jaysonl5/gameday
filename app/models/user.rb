1b1c5c2ba87ebd324ebcfdc820e3f4f9296c5a3b
Jayson Lewis <jayson.lewis5@gmail.com>
2025-12-29 21:42:32 -0600

    Working auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,29 @@ class User < ApplicationRecord
   rolify
 
   def self.from_omniauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_initialize do |u|
-      u.email = auth.info.email
-      u.password = Devise.friendly_token[0, 20]
-      u.name = auth.info.name
-      u.avatar_url = auth.info.image
-      u.approved = false  # Requires admin approval
+    # First, try to find user by provider and uid (existing OAuth user)
+    user = where(provider: auth.provider, uid: auth.uid).first
+
+    # If not found, try to find by email (existing email/password user)
+    user ||= where(email: auth.info.email).first
+
+    # If still not found, create a new user
+    if user.nil?
+      user = new(
+        provider: auth.provider,
+        uid: auth.uid,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        name: auth.info.name,
+        avatar_url: auth.info.image,
+        approved: false  # Requires admin approval
+      )
+    else
+      # Update existing user with OAuth info if they don't have it
+      user.provider = auth.provider if user.provider.blank?
+      user.uid = auth.uid if user.uid.blank?
+      user.name = auth.info.name if user.name.blank?
+      user.avatar_url = auth.info.image if user.avatar_url.blank?
     end
 
     user.save
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,7 @@
-OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.allowed_request_methods = %i[post get]
+
+# Configure OmniAuth to use Rails CSRF tokens
+OmniAuth.config.request_validation_phase = Proc.new do |env|
+  # This allows OmniAuth to bypass its internal CSRF check when Rails has already verified the token
+  # The Rails authenticity token is included automatically by button_to/form_for
+end
